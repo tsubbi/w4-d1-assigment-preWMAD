eed23e97b1cfa56c85261ccd958d72f8d4d2e59c
Jamie <jamie.yjchen@gmail.com>
2020-12-14 15:49:05 -0800

    Finish Sum of Cubes

--- sumOfCubes.js
+++ sumOfCubes.js
@@ -2,7 +2,11 @@
 // and returns the sum of its cubes.
 
 const sumOfCubes = function (arr) {
-
+    let result = 0;
+    for (i = 0; i < arr.length; i++) {
+        result += Math.pow(arr[i], 3);
+    }
+    return result;
 }
 
 console.log(sumOfCubes([3, 4, 5]));
