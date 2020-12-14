cd8d0193123542d70a930969e91568b798b22cfb
Jamie <jamie.yjchen@gmail.com>
2020-12-14 15:30:17 -0800

    Finish Add Larget Numbers

--- addLargestNumbers.js
+++ addLargestNumbers.js
@@ -1,8 +1,18 @@
 // function will take in an array, and add the two biggest numbers in the array
 
 const addLargestNumbers = function(data) {
-    // Put your solution here
-  };
+  if (data.length < 2) {
+    return "Improper Data"
+  }
+  let max1 = Math.max(...data);
+  let max2 = 0;
+  for (i = 0; i < data.length; i++) {
+    if (max2 < data[i] && data[i] < max1) {
+      max2 = data[i];
+    }
+  }
+  return max1 + max2;
+};
   
   console.log(addLargestNumbers([1, 10]));
   // should return 11
